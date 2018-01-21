65198f55ba57d7954145740344bbf17cc3c5b0c6
lyliux <35312601+lyliux@users.noreply.github.com>
2018-01-22 10:08:34 +1100

    Update const.java

--- src/main/java/frc/team6434/robot/const.java
+++ src/main/java/frc/team6434/robot/const.java
@@ -2,5 +2,19 @@ package frc.team6434.robot;
 
 protected class constants()
 {
+  //Assistive Climb
   protected double AMS = 0.5; //Assitive Motor Speed
+  
+  //Climb
+  
+  
+  //Drivetrain
+  
+  
+  //Intake
+  
+  
+  //Lift
+  
+  
 }
